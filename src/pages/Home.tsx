7bb437906221b467a1bd2542e3ea737415c4f17e
patricia <59278560+Patriciapavia@users.noreply.github.com>
2022-10-13 21:30:20 +1100

    create home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,205 @@
+import React, { useState } from "react";
+import { Grid, Snackbar, SnackbarOrigin, TextField } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { useSelector, useDispatch } from "react-redux";
+import { addTrip } from "../feature/Trip";
+import { Select, SelectOption } from "../components/Select";
+import { countries } from "../utils/countryArray";
+import { RootState } from "../store";
+import { validate } from "../components/Validator";
+export interface State extends SnackbarOrigin {
+  open: boolean;
+}
+
+interface Key {
+  connected: boolean;
+  type: string;
+}
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  papper: {
+    padding: "20px",
+    width: "60vh",
+    height: "50vh",
+    backgroundColor: "#ECF0F4",
+  },
+
+  formWrap: {
+    backgroundColor: "white",
+    padding: "20px 0px 20px 50px",
+
+    maxWidth: "400px",
+  },
+}));
+
+function Home() {
+  const dispatch = useDispatch();
+  const tripList = useSelector((state: RootState) => state.trips.value);
+  const [countrySelected, setCountrySelected] = useState<SelectOption[]>([]);
+  const [name, setName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [key, setKey] = useState(false);
+  const [error, setError] = useState("");
+  const [message, setMessage] = useState("");
+  const [snackbar, setSnackbar] = React.useState<State>({
+    open: false,
+    vertical: "top",
+    horizontal: "center",
+  });
+  console.log(startDate);
+
+  const destination = countrySelected.map(function (obj) {
+    return obj.name;
+  });
+
+  const { vertical, horizontal, open } = snackbar;
+
+  const addTripHandler = (event: any) => {
+    event.preventDefault();
+
+    const valid = validate(name, setError, startDate, endDate, destination);
+
+    if (valid) {
+      setError("");
+      setMessage("");
+      dispatch(
+        addTrip({
+          id: tripList.length + 1,
+          name,
+          startDate,
+          endDate,
+          destination,
+          status: "NOT_STARTED",
+        })
+      );
+      const resetForm = async () => {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        setName("");
+        setEndDate("");
+        setStartDate("");
+        setKey((k) => !k);
+      };
+      setMessage("Trip successfully added");
+
+      resetForm();
+      setSnackbar({ open: true, vertical: "top", horizontal: "center" });
+    }
+  };
+
+  function handleChange(event: any) {
+    const value = event.target.value;
+    if (!value.includes("_")) setError("You cannot use an underscore");
+    else setError("");
+    setMessage(value);
+  }
+
+  const { formWrap } = useStyles();
+
+  return (
+    <>
+      <div className={formWrap}>
+        <div style={{ textAlign: "center" }}>
+          {" "}
+          <h2>welcome to Freely</h2>
+        </div>
+        {error && (
+          <div style={{ color: "red", textAlign: "center" }}>{error}</div>
+        )}
+        <form onSubmit={addTripHandler}>
+          <Snackbar
+            anchorOrigin={{ vertical, horizontal }}
+            open={open}
+            message={message}
+            autoHideDuration={1000}
+            key={vertical + horizontal}
+          />
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <label>Title:</label>
+            </Grid>
+            <Grid item xs={9}>
+              <input
+                style={{ width: "210px" }}
+                type="text"
+                value={name}
+                placeholder="Name..."
+                onChange={(event) => {
+                  setName(event.target.value);
+                }}
+              />
+            </Grid>
+          </Grid>
+          <br></br>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <label> Start Date:</label>
+            </Grid>
+            <Grid item xs={9}>
+              <input
+                style={{ width: "210px" }}
+                // key={key}
+                type="date"
+                onChange={(event) => {
+                  setStartDate(event.target.value);
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <br></br>
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <label> End Date:</label>
+            </Grid>
+            <Grid item xs={9}>
+              <input
+                style={{ width: "210px" }}
+                // key={key}
+                type="date"
+                onChange={(event) => {
+                  setEndDate(event.target.value);
+                }}
+              />
+            </Grid>
+          </Grid>
+
+          <br></br>
+
+          <Grid container spacing={2}>
+            <Grid item xs={3}>
+              <label> Destination</label>
+            </Grid>
+            <Grid item xs={9}>
+              <Select
+                multiple
+                options={countries}
+                value={countrySelected}
+                onChange={(o) => setCountrySelected(o)}
+              />
+            </Grid>
+          </Grid>
+
+          <br />
+          <Grid container spacing={2}>
+            <Grid item xs={8}></Grid>
+            <Grid item xs={4}>
+              <button
+                style={{ borderRadius: " 15px 15px", padding: "3px 8px" }}
+                type="submit"
+              >
+                {" "}
+                Submit
+              </button>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default Home;
