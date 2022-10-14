f4f9eb40627eac05f045a9a9e794892ebd69118e
patricia <59278560+Patriciapavia@users.noreply.github.com>
2022-10-14 11:57:11 +1100

    add navbar active

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,44 @@
 import React, { useState } from "react";
-
+import { Button } from "@material-ui/core";
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
 function Navbar() {
+  const [activeMenu, setActiveMenu] = useState("");
+
   return (
     <>
       <nav className={"nav-menu"}>
         <ul>
           <li className="navbar-toggle">
-            <Link to="/" className="menu-bars">
-              Home
-            </Link>
+            <button style={{ padding: "8px 40px", backgroundColor: "black" }}>
+              <Link
+                className={
+                  activeMenu === "/" ? "active menu-bars" : "menu-bars"
+                }
+                onClick={() => {
+                  setActiveMenu("/");
+                }}
+                to="/"
+              >
+                Home
+              </Link>
+            </button>{" "}
           </li>
           <li className="navbar-toggle active">
-            <Link to="/trips" className="menu-bars">
-              Trips
-            </Link>
+            <button style={{ padding: "8px 40px", backgroundColor: "black" }}>
+              <Link
+                onClick={() => {
+                  setActiveMenu("trips");
+                }}
+                to="/trips"
+                className={
+                  activeMenu === "trips" ? "active menu-bars" : "menu-bars"
+                }
+              >
+                Trips
+              </Link>
+            </button>
           </li>
         </ul>
       </nav>
